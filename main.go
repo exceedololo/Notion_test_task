92e76e6e7a87949d551a569a908991bdac51c91d
exceedololo <exceedololo@outlook.com>
2023-05-13 11:57:58 +0300

    main branch

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+type IncrementInput struct {
+	Key   string `json:"key"`
+	Value int    `json:"value"`
+}
+
+type IncrementResult struct {
+	Value int `json:"value"`
+}
+
+var IncrementMap = make(map[string]int)
+
+func IncrementingStructFunc(inp IncrementInput) IncrementResult {
+	return 0
+}
+
 func main() {
 
 }
