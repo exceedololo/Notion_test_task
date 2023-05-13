fd0fe2cdec939aa4056ee3ab98c9fa8315b2f287
exceedololo <exceedololo@outlook.com>
2023-05-13 15:50:54 +0300

    added incrementing funco

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+type RequestIn struct {
+	Key   string `json:'key'`
+	Value int    `json:'value'`
+}
+
+type RequestOut struct {
+	Value int `json:'value'`
+}
+
+var RequestMap = make(map[string]int)
+
+func WorkWithRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var req RequestIn
+	err := decoder.Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	value := RequestMap[req.Key] + req.Value
+	RequestMap[req.Key] = value
+	resp := RequestOut{Value: value}
+	json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+	port := "8080"
+	http.HandleFunc("/", WorkWithRequest)
+	log.Printf("Starting server on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
